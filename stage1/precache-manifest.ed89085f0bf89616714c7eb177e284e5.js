449fa34e448f8dd7b538addffdf29a492d5c7198
rogeraabbccdd <rogeraabbccdd@gmail.com>
2019-07-13 21:10:51 +0800

    第一關: 番茄鐘

---
+++ stage1/precache-manifest.ed89085f0bf89616714c7eb177e284e5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "cce7d88b862430b0eab21b77aac07590",
+    "url": "fonts/orbitron-v10-latin-regular.eot"
+  },
+  {
+    "revision": "4f8694418cb9fb39d57bc83703e253d2",
+    "url": "css/loading.css"
+  },
+  {
+    "revision": "7279306cc82af49933eeecc958c82144",
+    "url": "sound/clock.mp3"
+  },
+  {
+    "revision": "bda7bc4c888346a3a888",
+    "url": "js/chunk-vendors.2f981c75.js"
+  },
+  {
+    "revision": "116896ebf949417c55573da8387eee2e",
+    "url": "img/loading.116896eb.svg"
+  },
+  {
+    "revision": "b9d6f821f54d73e5eb520538bdbae7db",
+    "url": "img/hourglass.b9d6f821.svg"
+  },
+  {
+    "revision": "b0fc28b590787ac1c298513a13b793df",
+    "url": "img/diego-ph-222506-unsplash.b0fc28b5.jpg"
+  },
+  {
+    "revision": "40064a77b615f9760bee47cf639a5775",
+    "url": "img/cup.40064a77.svg"
+  },
+  {
+    "revision": "f8ef8b645346bb316f3ea7075144b7b9",
+    "url": "index.html"
+  },
+  {
+    "revision": "4f1a9da5b8498adc0ac1",
+    "url": "js/app.b626e121.js"
+  },
+  {
+    "revision": "fc21fbb31ceb9691329d902abf2e2eca",
+    "url": "css/orbitron.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "117a3d1ebc6cfff415368f09bdf87055",
+    "url": "fonts/orbitron-v10-latin-regular.woff2"
+  },
+  {
+    "revision": "967e2419ec783a764a1875258ce3e3f7",
+    "url": "fonts/orbitron-v10-latin-regular.woff"
+  },
+  {
+    "revision": "b917c4211e212c11985dc8bdf256100d",
+    "url": "fonts/orbitron-v10-latin-regular.ttf"
+  },
+  {
+    "revision": "94dee9368288c976e54b558df65f2436",
+    "url": "fonts/orbitron-v10-latin-regular.svg"
+  },
+  {
+    "revision": "bda7bc4c888346a3a888",
+    "url": "css/chunk-vendors.a0a1ba72.css"
+  },
+  {
+    "revision": "4f1a9da5b8498adc0ac1",
+    "url": "css/app.cca8e268.css"
+  }
+];
