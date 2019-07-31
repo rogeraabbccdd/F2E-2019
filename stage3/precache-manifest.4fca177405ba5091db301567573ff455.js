ce9256df1cb35e953507c9c96632402bbb66b10a
rogeraabbccdd <rogeraabbccdd@gmail.com>
2019-07-31 13:04:29 +0800

    第三關 - MP3 Player

---
+++ stage3/precache-manifest.4fca177405ba5091db301567573ff455.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ec9a14346e793d236a37afe929409f4",
+    "url": "img/Search.1ec9a143.svg"
+  },
+  {
+    "revision": "107714f7e53099bf4d66c9ad8146a15a",
+    "url": "music/redline.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "eb3b423bdd261584939673f0776be5ed",
+    "url": "index.html"
+  },
+  {
+    "revision": "9bdd9c18cfee4720f74e",
+    "url": "js/app.44f37f2d.js"
+  },
+  {
+    "revision": "5ac070885adaf1c147e7",
+    "url": "js/chunk-vendors.07035d77.js"
+  },
+  {
+    "revision": "28898e7637745f4067577f1b8eccc855",
+    "url": "music/dreams.mp3"
+  },
+  {
+    "revision": "d3e56a5cdd2f36948ea2f3b613dd208e",
+    "url": "music/coveritup.mp3"
+  },
+  {
+    "revision": "64fa8904cc83c7f78c6dc3207202b413",
+    "url": "js/loading-bar.js"
+  },
+  {
+    "revision": "27a07c669b31be9a532bc8e5284ee810",
+    "url": "img/BlackCover2.svg"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "fonts/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "61c3eec5494a7fbf9c56",
+    "url": "js/about.91a60a62.js"
+  },
+  {
+    "revision": "5508c33d3b15352afe07aa33851fb5cb",
+    "url": "img/Settings.5508c33d.svg"
+  },
+  {
+    "revision": "72b309bcb1cb151b0449ddd753a85837",
+    "url": "img/002-music.72b309bc.svg"
+  },
+  {
+    "revision": "d885ef6e676f5ca38a0c785afcf6a42c",
+    "url": "img/BlackCover.svg"
+  },
+  {
+    "revision": "c9c5a53143793b1faaf95e5026dbe168",
+    "url": "img/left-arrow-angle-1.c9c5a531.svg"
+  },
+  {
+    "revision": "1724e64f4185e9f6a5f0c5c5edc906cd",
+    "url": "img/disc.png"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "4a5569962dc210bac72543b39b347a0e",
+    "url": "css/loading-bar.css"
+  },
+  {
+    "revision": "5ac070885adaf1c147e7",
+    "url": "css/chunk-vendors.ab429506.css"
+  },
+  {
+    "revision": "9bdd9c18cfee4720f74e",
+    "url": "css/app.bf66da69.css"
+  },
+  {
+    "revision": "61c3eec5494a7fbf9c56",
+    "url": "css/about.2a20278c.css"
+  },
+  {
+    "revision": "ddc1966d567284fb89f3159d1dbbac7e",
+    "url": "album/ncs.jpg"
+  }
+];
