a9c47577d5fa220cfc3115cd7d9e0bf0225ea7c3
rogeraabbccdd <rogeraabbccdd@gmail.com>
2019-08-05 15:55:13 +0800

    第四關 - 線上支付

---
+++ stage4/precache-manifest.05db601bd3d9a36985cf234eddb17b6a.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "81421dd5cb5cf2865ae7ce4d4ef5bda3",
+    "url": "fonts/noto-sans-tc-v9-chinese-traditional-regular.eot"
+  },
+  {
+    "revision": "1e99b3cf2a08a6c12357e173b632c962",
+    "url": "fonts/noto-sans-tc-v9-chinese-traditional-regular.woff"
+  },
+  {
+    "revision": "676c3fcb482bbe912b24",
+    "url": "js/chunk-vendors.709bcbc6.js"
+  },
+  {
+    "revision": "aeea8cf85a093d6e6a6ef036f22b34e6",
+    "url": "img/icon_atm.aeea8cf8.svg"
+  },
+  {
+    "revision": "45904667ad7e13405062566fe6b6f58b",
+    "url": "img/icon_card.45904667.svg"
+  },
+  {
+    "revision": "c2551afbced6c7594483976f05946dc0",
+    "url": "img/logo.c2551afb.svg"
+  },
+  {
+    "revision": "3c75df31bbd5bb14a515407299e77132",
+    "url": "img/step3.3c75df31.svg"
+  },
+  {
+    "revision": "16e05fd13267582eab985ba30f092899",
+    "url": "img/Group 40.16e05fd1.svg"
+  },
+  {
+    "revision": "d90355e892ca22809f39cae5010ace67",
+    "url": "img/line_pay.d90355e8.svg"
+  },
+  {
+    "revision": "1f53e6c70760cbd892254dcdbc6df35f",
+    "url": "img/unionpay.1f53e6c7.png"
+  },
+  {
+    "revision": "f59d456e0e732071447b9bea7cd06cc4",
+    "url": "img/icon_store.f59d456e.svg"
+  },
+  {
+    "revision": "86ae865672b084fb7cea921b01bc0311",
+    "url": "img/icon_done.86ae8656.svg"
+  },
+  {
+    "revision": "5ad7972e73e1da3f34d0",
+    "url": "js/app.a12a3995.js"
+  },
+  {
+    "revision": "5d98112e90b726aee70b63e6b0ca0c66",
+    "url": "fonts/noto-sans-tc-v9-chinese-traditional-regular.woff2"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "44d80edbb024d08c79042218e7277f68",
+    "url": "fonts/noto-sans-tc-v9-chinese-traditional-regular.svg"
+  },
+  {
+    "revision": "2c14cf930833b2314642742f1c221031",
+    "url": "img/Ellipse2.2c14cf93.svg"
+  },
+  {
+    "revision": "8ef308ece334f4d69937b6248ec2d841",
+    "url": "img/icon_arrow.8ef308ec.svg"
+  },
+  {
+    "revision": "ec8e05877da4e163ba8cb514682bbe0a",
+    "url": "img/step2.ec8e0587.svg"
+  },
+  {
+    "revision": "90463964250eeb29c556aa983dfde06d",
+    "url": "index.html"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "fonts/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "5fef99c20ac7e1fa2861792ef62f8f14",
+    "url": "css/noto.css"
+  },
+  {
+    "revision": "676c3fcb482bbe912b24",
+    "url": "css/chunk-vendors.dc9e8669.css"
+  },
+  {
+    "revision": "5ad7972e73e1da3f34d0",
+    "url": "css/app.a3e579cc.css"
+  }
+];
