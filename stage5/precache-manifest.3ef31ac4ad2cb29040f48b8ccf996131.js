037f811abaf7c5de2125bcb012159c47c271b2f5
rogeraabbccdd <rogeraabbccdd@gmail.com>
2019-08-12 16:19:32 +0800

    第五關 - 90 秒挑戰遊戲

---
+++ stage5/precache-manifest.3ef31ac4ad2cb29040f48b8ccf996131.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "a9ea653e91651732c6b4b25d288efe8c",
+    "url": "img/背景＿高雄發大財.jpg"
+  },
+  {
+    "revision": "17b8167863ea15ced02d12cc3d6012a5",
+    "url": "img/高雄發大財.png"
+  },
+  {
+    "revision": "fe981bbed49f6433ae24",
+    "url": "js/chunk-vendors.5c737f9f.js"
+  },
+  {
+    "revision": "9223fdaf7fa58870fe862bbfe5e2ac9a",
+    "url": "index.html"
+  },
+  {
+    "revision": "47ec63131e3976c6304648bf9dcef03d",
+    "url": "img/蚊子.svg"
+  },
+  {
+    "revision": "cdef11f336e5bf5ffb92c5c59ec57d78",
+    "url": "img/韓導2.svg"
+  },
+  {
+    "revision": "f02ca9d1773248dac78403688aaf701e",
+    "url": "img/韓導1.svg"
+  },
+  {
+    "revision": "79bac8daf2a0beee9d22dae7d343455f",
+    "url": "img/韓導3.svg"
+  },
+  {
+    "revision": "4c9b4d969e31e12b4e04",
+    "url": "js/app.142572e9.js"
+  },
+  {
+    "revision": "e9691b43c3781e8c616aac35cf266343",
+    "url": "img/閃亮亮.svg"
+  },
+  {
+    "revision": "ffb9a1557879d9b5f122fce61e973e11",
+    "url": "img/金幣數.svg"
+  },
+  {
+    "revision": "e80fa9791de2a5c227543e24d2a32a3f",
+    "url": "img/金幣.svg"
+  },
+  {
+    "revision": "1f26900277d02b231b5bab0745e914ab",
+    "url": "img/彩帶.svg"
+  },
+  {
+    "revision": "c1818906056fb99757c4c7abe1daa6ba",
+    "url": "img/石頭.svg"
+  },
+  {
+    "revision": "cca5e8de858107838c299b8815dc1280",
+    "url": "img/碰.svg"
+  },
+  {
+    "revision": "27a04d87daa9e83f9105e9914c4b8fd0",
+    "url": "img/背景＿又老又窮.jpg"
+  },
+  {
+    "revision": "e0f9f1f66afc5bc8eefc40cd255d0b00",
+    "url": "img/老郭.svg"
+  },
+  {
+    "revision": "9e22cb7544cfb2e18a3409b83edc7102",
+    "url": "img/老蔡.svg"
+  },
+  {
+    "revision": "d3a7be2193705dc0a36724c7b5884c06",
+    "url": "img/神秘小幫手.svg"
+  },
+  {
+    "revision": "f2a12811d80429bf5cce8485878efa33",
+    "url": "img/終點線.svg"
+  },
+  {
+    "revision": "ba5eac12267d7ee6599eb87616241ae4",
+    "url": "img/吃中天菇.svg"
+  },
+  {
+    "revision": "4e1df1e690f017710fcc337572e6b4ae",
+    "url": "img/生命.svg"
+  },
+  {
+    "revision": "288634e30b7378923047f52923bb25ef",
+    "url": "img/發大財.svg"
+  },
+  {
+    "revision": "217a0000d12f48c6d092237060704572",
+    "url": "img/無敵鋼鐵韓導.svg"
+  },
+  {
+    "revision": "fa5631c9ca1c048a58f4887e655389d2",
+    "url": "img/淹大水.svg"
+  },
+  {
+    "revision": "1d4d8d2bb713e43814e2a64ab9b671b4",
+    "url": "img/無敵星星.svg"
+  },
+  {
+    "revision": "5fcc0668acc111a921462445dfb3e609",
+    "url": "img/向下鍵.svg"
+  },
+  {
+    "revision": "a4a0c452f2b973f5940e1631d5a56df3",
+    "url": "img/向上鍵.svg"
+  },
+  {
+    "revision": "d9dd65faf75a89c817d555d809d9b3f7",
+    "url": "img/吃金幣.svg"
+  },
+  {
+    "revision": "1b976ba16d9543f5eb1440e58ab63035",
+    "url": "img/吃星星.svg"
+  },
+  {
+    "revision": "e808ad1940ba65a33d2a597c471c0056",
+    "url": "img/中天菇.svg"
+  },
+  {
+    "revision": "bd25160956dc05241be5fc1c753468b7",
+    "url": "img/warning.svg"
+  },
+  {
+    "revision": "3605bba70bfd6d412cfea2fa9b57ac01",
+    "url": "img/1450.svg"
+  },
+  {
+    "revision": "fe981bbed49f6433ae24",
+    "url": "css/chunk-vendors.3a837761.css"
+  },
+  {
+    "revision": "4c9b4d969e31e12b4e04",
+    "url": "css/app.81a081e4.css"
+  }
+];
