f0cff35e8e0cee129d145b4e187bb22c69936ccf
Kento <rogeraabbccdd@gmail.com>
2019-08-19 15:29:35 +0800

    第六關 - 旅館預約服務
    第六關 - 旅館預約服務
    
    
    第六關 - 旅館預約服務

---
+++ stage6/precache-manifest.e170b6b8343b3226c511a9b226723a6a.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b2d4309989694058757191218de08d2",
+    "url": "img/Lovepik_com-400275415-willow-branch.png"
+  },
+  {
+    "revision": "8f445ef47667f1e421c4b748deefaa75",
+    "url": "img/1@2x.png"
+  },
+  {
+    "revision": "2ae93a9b681affca9bac0ac51e50a96c",
+    "url": "img/photo-1501876725168-00c445821c9e@2x.png"
+  },
+  {
+    "revision": "2cb8d7dffbbe5cf0bbff",
+    "url": "js/chunk-vendors.b2a81b4d.js"
+  },
+  {
+    "revision": "e73c0ef96e056e9a75168df4942e89d4",
+    "url": "index.html"
+  },
+  {
+    "revision": "6448dde87ab388c7272728ba16f59c36",
+    "url": "img/photo-1519974719765-e6559eac2575@2x.png"
+  },
+  {
+    "revision": "ba7f2318ee1161e8739c234c0fbeccba",
+    "url": "img/photo-1552902019-ebcd97aa9aa0@2x.png"
+  },
+  {
+    "revision": "4725350b36818149163b94b49c26f41c",
+    "url": "img/220px-QRcode_image.svg.png"
+  },
+  {
+    "revision": "4051c0cdb6cf6d8d8212e6d906ca27c9",
+    "url": "img/220px-QRcode_image.svg@2x.png"
+  },
+  {
+    "revision": "f76f775dd3ae18333fe2682bb2288cae",
+    "url": "img/logo.svg"
+  },
+  {
+    "revision": "e2296db9734d879f96a7c1ab620fe449",
+    "url": "img/Mask Group 2.svg"
+  },
+  {
+    "revision": "ea39f9b799fc7868dec95283584106be",
+    "url": "img/Mask Group 3.svg"
+  },
+  {
+    "revision": "9013f41d880ea94fbd9a0ff49a8dc1c3",
+    "url": "img/Mask Group 4.svg"
+  },
+  {
+    "revision": "3eab39c3b35893f68cac9a558f74824f",
+    "url": "img/Mask Group 5.svg"
+  },
+  {
+    "revision": "63b7eebdd511cb840ddfbeb58ec8a6ad",
+    "url": "img/Mask Group 6.svg"
+  },
+  {
+    "revision": "45450a6dc0497b72ca1539b6a27b12b5",
+    "url": "img/Mask Group 7.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "b36dda500fcc8d16e10bbae4f5cc8caa",
+    "url": "img/Mask Group 1.svg"
+  },
+  {
+    "revision": "ff021d3cdceaf6e5fc5a",
+    "url": "js/app.4ea355e3.js"
+  },
+  {
+    "revision": "52133b8a7ac6069af0bb52cdec616f90",
+    "url": "img/photo-1526880792616-4217886b9dc2.png"
+  },
+  {
+    "revision": "45cfb4c5b959a82c9032506431b02f6e",
+    "url": "img/photo-1519974719765-e6559eac2575.png"
+  },
+  {
+    "revision": "0956605a1fad2a2aa7367851cdee2ce7",
+    "url": "img/photo-1501127122-f385ca6ddd9d.png"
+  },
+  {
+    "revision": "f5979eb103c5667bd3ce47e4c781e1a5",
+    "url": "img/photo-1552902019-ebcd97aa9aa0.png"
+  },
+  {
+    "revision": "d07babc7d57a0a3d0205bb1a0173ecb6",
+    "url": "img/photo-1558211583-03ed8a0b3d5f.png"
+  },
+  {
+    "revision": "ee80db4f097895e41f30162ef6f9b9bb",
+    "url": "img/photo-1501876725168-00c445821c9e.png"
+  },
+  {
+    "revision": "fa46d77844fe7927e95a9d72eff8e824",
+    "url": "img/Lovepik_com-400275415-willow-branch@2x.png"
+  },
+  {
+    "revision": "5f762a0fb2254802499340752c687463",
+    "url": "img/photo-1526880792616-4217886b9dc2@2x.png"
+  },
+  {
+    "revision": "c45baa69ed6f3fa60e2c114f37e8d984",
+    "url": "img/photo-1501127122-f385ca6ddd9d@2x.png"
+  },
+  {
+    "revision": "380c2ace8e4f04158e45b2811bfff5f0",
+    "url": "img/photo-1558211583-03ed8a0b3d5f@2x.png"
+  },
+  {
+    "revision": "2cf8a4d6637e4aaaa2fee31a2bee400a",
+    "url": "img/1.png"
+  },
+  {
+    "revision": "5d98112e90b726aee70b63e6b0ca0c66",
+    "url": "fonts/noto-sans-tc-v9-chinese-traditional-regular.woff2"
+  },
+  {
+    "revision": "1e99b3cf2a08a6c12357e173b632c962",
+    "url": "fonts/noto-sans-tc-v9-chinese-traditional-regular.woff"
+  },
+  {
+    "revision": "44d80edbb024d08c79042218e7277f68",
+    "url": "fonts/noto-sans-tc-v9-chinese-traditional-regular.svg"
+  },
+  {
+    "revision": "81421dd5cb5cf2865ae7ce4d4ef5bda3",
+    "url": "fonts/noto-sans-tc-v9-chinese-traditional-regular.eot"
+  },
+  {
+    "revision": "5fef99c20ac7e1fa2861792ef62f8f14",
+    "url": "css/noto.css"
+  },
+  {
+    "revision": "2cb8d7dffbbe5cf0bbff",
+    "url": "css/chunk-vendors.ee7084df.css"
+  },
+  {
+    "revision": "ff021d3cdceaf6e5fc5a",
+    "url": "css/app.9ac1debf.css"
+  }
+];
